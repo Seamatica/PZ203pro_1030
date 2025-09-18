173dc98fa5187e4dacbae34dac025a3914fb75bb
WeiHe <1998howei@gmail.com>
2025-09-18 11:16:34 -0230

    Added GPS message in PL and PS

---
+++ README.md
@@ -0,0 +1,15 @@
+## How to Setup Project
+
+### Run these Commands in CMD prompt one by one within this directory:
+- "C:\Xilinx\Vivado\2019.1\settings64.bat" && vivado -mode tcl
+    - Find your specific path for settings64.bat   
+- source PZ_PZP201_PRO_NO_OS.tcl
+
+### Open the new folder PZ_PZP201_PRO_NO_OS, open the vivado project, generate the bitstream.
+
+### After bitstream generation
+- In Vivado, export hardware, then launch SDK
+- In SDK, click File/New/Application Project
+- Put a descriptive project name (say "test"), click Next
+- Choose Empty Application, click Finish
+- Copy the files in sdk_source folder, paste them into test/src folder.
---
+++ library/gps_message_1.0/hdl/gps_message_v1_0.v
@@ -0,0 +1,96 @@
+
+`timescale 1 ns / 1 ps
+
+	module gps_message_v1_0 #
+	(
+		// Users to add parameters here
+
+		// User parameters ends
+		// Do not modify the parameters beyond this line
+
+
+		// Parameters of Axi Slave Bus Interface S00_AXI
+		parameter integer C_S00_AXI_DATA_WIDTH	= 32,
+		parameter integer C_S00_AXI_ADDR_WIDTH	= 5
+	)
+	(
+		// Users to add ports here
+        output wire [23:0] o_gps_utc_time,
+        output wire [15:0] o_gps_date,
+        output wire [31:0] o_gps_latitude,
+        output wire [31:0] o_gps_longitude,
+        output wire [23:0] o_gps_ellipsoid_height,
+        output wire [15:0] o_gps_ground_speed,
+        output wire [11:0] o_gps_ground_heading,
+        output wire [3:0]  o_gps_type,
+        output wire        o_gps_new_data_strobe, // Single-cycle pulse
+		// User ports ends
+		// Do not modify the ports beyond this line
+
+
+		// Ports of Axi Slave Bus Interface S00_AXI
+		input wire  s00_axi_aclk,
+		input wire  s00_axi_aresetn,
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+		input wire [2 : 0] s00_axi_awprot,
+		input wire  s00_axi_awvalid,
+		output wire  s00_axi_awready,
+		input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+		input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+		input wire  s00_axi_wvalid,
+		output wire  s00_axi_wready,
+		output wire [1 : 0] s00_axi_bresp,
+		output wire  s00_axi_bvalid,
+		input wire  s00_axi_bready,
+		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+		input wire [2 : 0] s00_axi_arprot,
+		input wire  s00_axi_arvalid,
+		output wire  s00_axi_arready,
+		output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+		output wire [1 : 0] s00_axi_rresp,
+		output wire  s00_axi_rvalid,
+		input wire  s00_axi_rready
+	);
+// Instantiation of Axi Bus Interface S00_AXI
+	gps_message_v1_0_S00_AXI # ( 
+		.C_S_AXI_DATA_WIDTH(C_S00_AXI_DATA_WIDTH),
+		.C_S_AXI_ADDR_WIDTH(C_S00_AXI_ADDR_WIDTH)
+	) gps_message_v1_0_S00_AXI_inst (
+		.S_AXI_ACLK(s00_axi_aclk),
+		.S_AXI_ARESETN(s00_axi_aresetn),
+		.S_AXI_AWADDR(s00_axi_awaddr),
+		.S_AXI_AWPROT(s00_axi_awprot),
+		.S_AXI_AWVALID(s00_axi_awvalid),
+		.S_AXI_AWREADY(s00_axi_awready),
+		.S_AXI_WDATA(s00_axi_wdata),
+		.S_AXI_WSTRB(s00_axi_wstrb),
+		.S_AXI_WVALID(s00_axi_wvalid),
+		.S_AXI_WREADY(s00_axi_wready),
+		.S_AXI_BRESP(s00_axi_bresp),
+		.S_AXI_BVALID(s00_axi_bvalid),
+		.S_AXI_BREADY(s00_axi_bready),
+		.S_AXI_ARADDR(s00_axi_araddr),
+		.S_AXI_ARPROT(s00_axi_arprot),
+		.S_AXI_ARVALID(s00_axi_arvalid),
+		.S_AXI_ARREADY(s00_axi_arready),
+		.S_AXI_RDATA(s00_axi_rdata),
+		.S_AXI_RRESP(s00_axi_rresp),
+		.S_AXI_RVALID(s00_axi_rvalid),
+		.S_AXI_RREADY(s00_axi_rready),
+		
+        .O_GPS_UTC_TIME(o_gps_utc_time),
+        .O_GPS_DATE(o_gps_date),
+        .O_GPS_LATITUDE(o_gps_latitude),
+        .O_GPS_LONGITUDE(o_gps_longitude),
+        .O_GPS_ELLIPSOID_HEIGHT(o_gps_ellipsoid_height),
+        .O_GPS_GROUND_SPEED(o_gps_ground_speed),
+        .O_GPS_GROUND_HEADING(o_gps_ground_heading),
+        .O_GPS_TYPE(o_gps_type),
+        .O_GPS_NEW_DATA_STROBE(o_gps_new_data_strobe)
+	);
+
+	// Add user logic here
+
+	// User logic ends
+
+	endmodule
